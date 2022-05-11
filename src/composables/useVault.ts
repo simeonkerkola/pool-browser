29a6866e9875a1370807644155da2725ed981be0
sssmi <simeon.kerkola@gmail.com>
2022-05-11 08:09:23 +0300

    Lint

--- src/composables/useVault.ts
+++ src/composables/useVault.ts
@@ -1,12 +1,12 @@
-import { ethers } from "ethers";
-import { Vault__factory } from "@balancer-labs/typechain";
+import { ethers } from 'ethers';
+import { Vault__factory } from '@balancer-labs/typechain';
 
 const provider = new ethers.providers.JsonRpcProvider(
-  "https://mainnet.infura.io/v3/" + import.meta.env.VITE_APP_INFURA_API_KEY
+  'https://mainnet.infura.io/v3/' + import.meta.env.VITE_APP_INFURA_API_KEY
 );
 
 // Vault contract address
-const address = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
+const address = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
 const vault = Vault__factory.connect(address, provider);
 
 export default function useVault() {
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,18 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-const PoolView = () => import("../views/PoolView.vue");
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+const PoolView = () => import('../views/PoolView.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
     },
     {
-      path: "/pool/:id",
-      name: "pool",
+      path: '/pool/:id',
+      name: 'pool',
       component: PoolView,
     },
   ],
